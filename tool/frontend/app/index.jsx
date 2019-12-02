986b18ed495bbd85669cb069b4465a5559905514
Andrey Lebedev <andrey@lebedev.lt>
2019-12-02 23:05:29 +0200

    Drop the "tool"

--- tool/frontend/app/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import 'typeface-roboto';
-
-import TrackerMainUI from './main.jsx';
-
-
-render(<TrackerMainUI/>, document.getElementById('app'));
